3ba28cc097dc3db6938341c50d78c4cf1a19f3f3
Dennis <jisheng.deng@gameconcourse.com>
2013-12-16 09:13:18 +0800

    Again again

--- portalWeb/js/index.js
+++ portalWeb/js/index.js
@@ -169,8 +169,8 @@ app.controller('newsSearchCtrl',function($scope,$http,$routeParams){
         }
     });
     $scope.mainlist={};
-    $http.post(Server + 'keyword/docPool',{filter:{keyword:$routeParams.keyword}}).success(function(d){
-        $scope.mainlist.list = d;
+    $http.post(Server + 'keyword/docPool/all',{filter:{keyword:$routeParams.keyword}}).success(function(d){
+        $scope.mainlist = d ;
     })
     $scope.pages = [1,2,3,4,5];
 
