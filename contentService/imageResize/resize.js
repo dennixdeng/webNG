728d4dc988f7e1fb74062e0cbb997a8a86b2b293
Dennis <jisheng.deng@gameconcourse.com>
2013-12-17 09:31:51 +0800

    hh

--- contentService/imageResize/resize.js
+++ contentService/imageResize/resize.js
@@ -36,3 +36,42 @@ getDBCollection('webNG_SUEU',['WebNG_users','publicUserPool','imagePool','attach
 var ossAPI = require('oss-client');
 var ossHost='oss.aliyuncs.com';
 //  var ossHost='oss-internal.aliyuncs.com';
+var oss = new ossAPI.OssClient({host:ossHost,accessKeyId: 'Ybx6lzed1szPRAuI',accessKeySecret: 'yoih8NiSadOlPJ9Syi65w7LdRsc6zA'});
+
+
+var fs = require('fs'),
+    request = require('request');
+
+var download = function(uri, filename){
+    request.head(uri, function(err, res, body){
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
+
+        request(uri).pipe(fs.createWriteStream(filename));
+    });
+};
+
+download('https://www.google.com/images/srpr/logo3w.png', 'google.png');
+
+var upload= function (fileId,path){
+    oss.putObject({bucket: 'webngimage-small',object: fileId,srcFile: path},
+        function (error, result) {
+            console.log(fileId + 'uploaded');
+        }
+    );
+};
+
+
+gm(imagePath)
+    .resize(640, 480)
+    .autoOrient()
+    .flip()
+    .write(newImagePath, function(e) {
+        if (e) throw e;
+        gm(newImagePath)
+            .resize(320, 240)
+            .write(newImagePathSmall, function(e) {
+                if (e) throw e;
+                console.log('resized successfuly');
+            });
+    });
